185240a4400b06899ef397ab20d08e8145b030bd
kkata <14849535+kkata@users.noreply.github.com>
2020-11-22 17:42:54 +0900

    use react-router

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import * as React from "react";
 import { render } from "react-dom";
 import styled, { createGlobalStyle } from "styled-components";
 import { Editor } from "./pages/editor";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { History } from "./pages/history";
 
 const GlobalStyle = createGlobalStyle`
   body * {
@@ -12,7 +19,15 @@ const GlobalStyle = createGlobalStyle`
 const Main = (
   <>
     <GlobalStyle />
-    <Editor />
+    <Router>
+      <Route exact path="/editor">
+        <Editor />
+      </Route>
+      <Route exact path="/history">
+        <History />
+      </Route>
+      <Redirect to="/editor" path="*" />
+    </Router>
   </>
 );
 
